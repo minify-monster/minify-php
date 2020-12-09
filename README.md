e3243e816c941edf8f50e8275f1ffa7d5cf97fef
Minify <support@minify.monster>
2020-12-09 22:46:28 +0100

    Commit library

--- README.md
+++ README.md
@@ -1,1 +1,105 @@
-minify-php
+# minify.monster API client for PHP
+
+PHP client for [minify.monster](https://minify.monster) services.
+
+## Usage
+
+**Request for Image URL:**
+
+````php
+$params = [
+	'url' => 'https://site-url.com/file.jpg',
+	'from_url' => true,
+	'images' => [
+		[
+		'mode' => 'auto',
+		'extension' => 'jpg',
+		'width' => 1920,
+		'height' => 1920
+		],[
+		'mode' => 'fill',
+		'extension' => 'webp',
+		'width' => 300,
+		'height' => 300
+		]
+	]
+]
+$monster = new Monster($apikey);
+$response = $monster->minify($params);
+````
+**Request for Image upload:**
+
+````php
+require_once 'minify.monster.php';
+$params = [
+	'url' => '/var/www/your-site/file.jpg',
+	'images' => [
+		[
+			'mode' => 'auto',
+			'extension' => 'jpg',
+			'width' => 1920,
+			'height' => 1920
+		],
+		[
+			'mode' => 'fill',
+			'extension' => 'webp',
+			'width' => 300,
+			'height' => 300
+		]
+	]
+]
+$monster = new Monster('your-api-key');
+$response = $monster->minify($params);
+````
+
+**Response:**
+
+````js
+{
+	'success': true,
+	'images': {
+		'https://storage.minify.monster/random-filename-for-file-1.jpg',
+		'https://storage.minify.monster/random-filename-for-file-2.webp'
+	}
+}
+````
+
+````js
+{
+	'success': false,
+	'message': 'error message'
+}
+````
+
+### Input parameters
+
+- `url` - image url or location on server
+- `from_url` - true if image url
+- `width` - in pixels (if not set, width will preserve)
+- `height` - in pixels (if not set, height will preserve)
+- `images` - array with requests for compression
+
+The `extension` property can have one of the following values:
+
+- `jpg`
+- `png`
+- `webp`
+
+The `quality` property can have one of the following values:
+- `auto` - default, if not set (auto quality is really recommended)
+- `X` - percentage (integer)
+
+The `mode` property can have one of the following values:
+
+- `auto` - default, if not set
+- `fill` - background with white color
+- `crop`
+
+The `method` property can have one of the following values:
+
+- `LANCZOS` - default, if not set
+- `NEAREST`
+- `BOX`
+- `BILINEAR`
+- `HAMMING`
+- `BICUBIC`
---
+++ minify.monster.php
@@ -0,0 +1,97 @@
+<?php
+class Monster {
+	protected $apiKey;
+	public function __construct($apiKey=NULL) {
+		$this->apiKey = $apiKey;
+	}
+	public function minify($params=NULL) {
+		if (empty($params)) {
+			$result=[
+				'success' => false,
+				'message' => 'empty params'
+			];
+		} else {
+			if ($params['from_url'] === true) {
+				$result = $this->transformImage($params);
+			} else {
+				if (file_exists($params['url'])) {
+					$upload = $this->uploadFile($params['url']);
+					if ($upload['success'] === true) {
+						$params = array_merge($params, ['apikey' => $this->apiKey, 'row' => $upload['row'], 'fingerprint' => $upload['fingerprint']]);
+						$result = $this->transformImage($params);
+					} else {
+						$result = $upload;
+					}
+				} else {
+					$result=['success'=>false, 'message'=>'file does not exists'];
+				}
+			}
+		}
+		return json_encode($result);
+	}
+	public function uploadFile($filename=NULL) {
+		if (class_exists('CURLFile')) {
+			$file = new CURLFile($filename);
+		} else {
+			$file = '@' . $filename;
+		}
+		$data = [
+			'apiKey' => $this->apiKey,
+			'file' => $file
+		];
+		$ch=curl_init();
+		curl_setopt($ch, CURLOPT_URL, 'https://upload.minify.monster');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+		$response = curl_exec($ch);
+		if (curl_errno($ch)) {
+			$curl_errno = curl_error($ch);
+		}
+		curl_close($ch);
+		if (isset($curl_errno)) {
+			$result=[
+				'success' => false,
+				'message' => $curl_errno
+			];
+		} else {
+			$result = json_decode($response, true);
+		}
+		return $result;
+	}
+	public function transformImage($input=NULL) {
+		$ch=curl_init();
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_URL, 'https://api.minify.monster');
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($input));
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+		$response = curl_exec($ch);
+		if (curl_errno($ch)) {
+			$curl_errno = curl_error($ch);
+		}
+		curl_close($ch);
+		if (isset($error_msg)) {
+			$pole=[
+				'success' => false,
+				'message' => $curl_errno
+			];
+		} else {
+			$vysledek = json_decode($response, true);
+			if ($vysledek['success'] === true) {
+				$pole=[
+					'success' => true,
+					'images' => $vysledek['images']
+				];
+			} else {
+				$pole=[
+					'success' => false,
+					'message' => $vysledek['message']
+				];
+			}
+		}
+		return $pole;
+	}
+}
